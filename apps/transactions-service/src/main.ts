cf9176cd885c68655a83219d92f2dd52b45a538a
Jora <jora_a@outlook.com>
2024-08-14 04:14:47 +0330

    Convert Http app to microservice

--- apps/transactions-service/src/main.ts
+++ apps/transactions-service/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { TransactionsServiceModule } from './transactions-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(TransactionsServiceModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(TransactionsServiceModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: process.env.NATS_URL,
+    },
+  });
+  await app.listen();
 }
 bootstrap();
