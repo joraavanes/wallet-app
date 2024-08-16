508132fc5eb4637edbbdce6f6d71ce7b9c1e9f61
Jora <jora_a@outlook.com>
2024-08-16 14:46:54 +0330

    Utilize db service in Transactions service module

--- apps/transactions-service/src/transactions-service.module.ts
+++ apps/transactions-service/src/transactions-service.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TransactionsServiceController } from './transactions-service.controller';
 import { TransactionsServiceService } from './transactions-service.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      port: +process.env.POSTGRES_PORT,
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+  ],
   controllers: [TransactionsServiceController],
   providers: [TransactionsServiceService],
 })
-export class TransactionsServiceModule {}
+export class TransactionsServiceModule { }
